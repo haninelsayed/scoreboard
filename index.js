6fbd8a7e8b95616cc130da8dae20f563ff7aebdf
haninelsayed <102517033+haninelsayed@users.noreply.github.com>
2022-09-13 00:03:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let scoreEl = document.getElementById("score1")
+let scoreEl2 = document.getElementById("score2")
+let scoreCount1 = 0
+let scoreCount2 = 0
+function addOne() {
+  scoreCount1 += 1
+  scoreEl.innerText = scoreCount1
+}
+function addTwo() {
+  scoreCount1 += 2
+  scoreEl.innerText = scoreCount1
+}
+function addThree() {
+  scoreCount1 += 3
+  scoreEl.innerText = scoreCount1
+}
+function addOneRe() {
+  scoreCount2 += 1
+  scoreEl2.innerText = scoreCount2
+}
+function addTwoRe() {
+  scoreCount2 += 2
+  scoreEl2.innerText = scoreCount2
+}
+function addThreeRe() {
+  scoreCount2 += 3
+  scoreEl2.innerText = scoreCount2
+}
+
+
